b65d92c67154571571d97f20cea3022b284cbae4
Laowang-BearPi <wangcheng@bearpi.cn>
2023-10-20 02:39:55 +0000

    update Hardware/LCD/HzLib.h.
    Signed-off-by: Laowang-BearPi <wangcheng@bearpi.cn>

--- Hardware/LCD/HzLib.h
+++ Hardware/LCD/HzLib.h
@@ -9,8 +9,8 @@ void GetGBKCode(unsigned char* pBuffer,unsigned char * c);
 
 
 //字体选择
-#define SONG_TYPEFACE                //选择宋体字
-//#define BOLDFACE                   //选择黑体字
+//#define SONG_TYPEFACE                //选择宋体字
+#define BOLDFACE                   //选择黑体字
 
 #endif 
 
